9f0925c326a4884f9ec92627a752c3a5bbc6e328
K M <kirill.maftuljak@khk.ee>
2021-09-08 22:14:21 +0300

    Tested framework working

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
 
-$app->get('/', function (Request $request, Response $response, $args) {
+$app->get('/', function (Request $request, Response $response) {
     $response->getBody()->write("Hello world!");
     return $response;
 });
