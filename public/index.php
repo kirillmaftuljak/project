a652bed023ae9ec249f4f4a22782e8714feba991
K M <kirill.maftuljak@khk.ee>
2021-09-08 22:38:54 +0300

    Tested routes and rotes parameters

--- public/index.php
+++ public/index.php
@@ -8,8 +8,21 @@ require __DIR__ . '/../vendor/autoload.php';
 $app = AppFactory::create();
 
 $app->get('/', function (Request $request, Response $response) {
-    $response->getBody()->write("Hello world!");
+    $response->getBody()->write("Hello Slim!");
     return $response;
 });
+
+$app->get('/hello/kadi', function (Request $request, Response $response) {
+    $response->getBody()->write("Hello Kadi!");
+    return $response;
+});
+
+$app->get('/hello/{name}', function (Request $request, Response $response, array $args)
+    {
+        $name = ucfirst($args['name']);
+        $response->getBody()->write(sprintf("Hello, %s!", $name));
+        return $response;
+});
+
 
 $app->run();
