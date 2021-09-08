560c704f59bb2fc6abca1d2b269eef69cf87e6bf
K M <kirill.maftuljak@khk.ee>
2021-09-08 22:55:58 +0300

    Installed and configured dependency inject

--- public/index.php
+++ public/index.php
@@ -2,27 +2,29 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use DI/composer
 
 require __DIR__ . '/../vendor/autoload.php';
 
+$container = new Container();
+
+AppFactory::setContainer($container);
+
+$continer->set('templating', function(){
+    return new Mustache_Engine([
+            'loader' => new Mustache_Loader_ProductionFilesystemLoader(
+        __DIR__'/../templates',['extension' => ''])
+    ]);
+
+});
+
 $app = AppFactory::create();
-
-$app->get('/', function (Request $request, Response $response) {
-    $response->getBody()->write("Hello Slim!");
-    return $response;
-});
-
-$app->get('/hello/kadi', function (Request $request, Response $response) {
-    $response->getBody()->write("Hello Kadi!");
-    return $response;
-});
-
-$app->get('/hello/{name}', function (Request $request, Response $response, array $args)
-    {
-        $name = ucfirst($args['name']);
-        $response->getBody()->write(sprintf("Hello, %s!", $name));
-        return $response;
-});
-
+    
+    $app->get('hello/{name}', function(Request $request, Response $response, array $args = []){
+        $html = $this->get('templating')->render('hello.html', ['name' => $args['name']
+    ]);
+        $response->getBody()->write($html);
+        return $response
+    })
 
 $app->run();
